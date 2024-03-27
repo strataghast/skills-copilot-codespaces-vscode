4e721bc61e356d8cadcb44874819e9f64846d768
Brett Simonds <122714358+strataghast@users.noreply.github.com>
2024-03-27 19:52:44 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,12 @@
+// Create web server
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Create a route
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start the server
+
